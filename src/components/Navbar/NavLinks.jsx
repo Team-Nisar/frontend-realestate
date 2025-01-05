d2acc133d216635440d187b965f32a966a64106c
Nisar Khan <khannisarnk0786@gmail.com>
2025-01-05 11:38:44 +0530

    api integration

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -13,32 +13,31 @@ const NavLinks = () => {
         <div>
           <div className="px-3 text-left md:cursor-pointer group">
             <h1
-              className="py-5 flex justify-between items-center border-b md:border-b-0 lg:border-b-0 xl:border-b-0 md:pr-0 pr-5 group"
+              className="flex items-center justify-between py-5 pr-5 border-b md:border-b-0 lg:border-b-0 xl:border-b-0 md:pr-0 group"
               onClick={() => {
                 heading !== link.name ? setHeading(link.name) : setHeading("");
                 setSubHeading("");
               }}
             >
               {link.name}
-              <span className="text-xl md:hidden inline">
+              <span className="inline text-xl md:hidden">
                 {heading === link.name ? <FiChevronUp size={30}/> : <FiChevronDown size={30}/>}
               </span>
-              <span className="text-xl md:mt-1 md:ml-2 md:block hidden group-hover:rotate-180">
+              <span className="hidden text-xl md:mt-1 md:ml-2 md:block group-hover:rotate-180">
                 <FiChevronDown/>
               </span>
             </h1>
             {link.submenu && (
               <div>
-                <div className="absolute top-16 hidden group-hover:md:block hover:md:block">
+                <div className="absolute hidden top-16 group-hover:md:block hover:md:block">
                   <div className="py-3">
                     <div
-                      className="w-4 h-4 left-10 absolute 
-                    mt-1 bg-black rotate-45"
+                      className="absolute w-4 h-4 mt-1 rotate-45 bg-black left-10"
                     ></div>
                   </div>
                   <div className={`bg-black mega-menu rounded-xl max-w-7xl min-w-3xl h-[41vh] p-5 flex justify-evenly gap-10`}>
                     {link.sublinks.map((mysublinks) => (
-                      <div className=" ">
+                      <div className="">
                         <h1 className="text-lg font-semibold">
                           {mysublinks.Head}
                         </h1>
@@ -47,7 +46,7 @@ const NavLinks = () => {
                             <li className="text-xs my-2.5">
                               <Link
                                 to={slink.link}
-                                className="hover:text-primary font-medium"
+                                className="font-medium hover:text-primary"
                               >
                                 {slink.name.slice(0,27)+`${slink.name.length > 25 ? "..." : ""}`}
                               </Link>
@@ -76,11 +75,11 @@ const NavLinks = () => {
                         ? setSubHeading(slinks.Head)
                         : setSubHeading("")
                     }
-                    className="py-4 pl-7 font-semibold md:pr-0 pr-5 flex justify-between items-center md:pr-0 pr-5"
+                    className="flex items-center justify-between py-4 pr-5 font-semibold pl-7 md:pr-0"
                   >
                     {slinks.Head}
 
-                    <span className="text-xl md:mt-1 md:ml-2 inline">
+                    <span className="inline text-xl md:mt-1 md:ml-2">
                       {subHeading === slinks.Head ? <FiMinus/> : <FiPlus/>}
                     </span>
                   </h1>
